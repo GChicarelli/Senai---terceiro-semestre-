31c5bdb5fc6592ec63e6961bfe1bc6c0431c527b
GIULIANO KELER CHICARELLI <53695322810@info.local>
2026-02-23 10:48:01 -0300

    API VH_Burguer Atualização de projeto

---
+++ VHburguer/DTOs/CategoriaDto/LerCategoriaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace VHBurguer.DTOs.CategoriaDto
+{
+    public class LerCategoriaDto
+    {
+        public int CategoriaID { get; set;  }
+        public string Nome { get; set; } = null;
+    }
+}
